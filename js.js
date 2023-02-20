2d21dbac2f895b130bf4daf5cf11b986210a3432
CritickalGames <31327715+CritickalGames@users.noreply.github.com>
2023-02-20 05:30:33 -0300

    s

--- js.js
+++ js.js
@@ -10,8 +10,7 @@ function eventos() {
 
 function carga() {
     displayResult("XArchivos/Botones","XArchivos/xBotones", "botones");
-    displayResult("XArchivos/1- el ABC del japonés, parte 1","XArchivos/xClases", "contenido");
-    alert("ka");
+    displayResult("XArchivos/"+$('select[name=clases]').val(),"XArchivos/xClases", "contenido");
 }
 
 
